357b251b440fe1181b9b0acb5c0f2c20965fda16
gileduardo <gilgea@gmail.com>
2025-11-02 19:37:17 -0300

    Views Blade - Imagem Logo - JQuery

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('templates/main',
+    [
+        'titulo'=>"Sistema Aula",
+        'cabecalho' => 'Home - Dashboard',
+        'rota' => '',
+    ]
+)
+
+@section('conteudo')
+
+@endsection
